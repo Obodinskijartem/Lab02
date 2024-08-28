44be97d81e2058c4928077292a8ef41a5050266b
Obodi <Obodi@DESKTOP-LDDTNBM>
2024-08-28 21:41:18 +0300

    Добавьте файлы проекта.

---
+++ Lab2App/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab2Lib;
+
+namespace Lab2App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Subscriber[] arrSubscribers;
+            Console.Write("Введіть кількість абонентів: ");
+            int cntSubscribers = int.Parse(Console.ReadLine());
+            arrSubscribers = new Subscriber[cntSubscribers];
+            for (int i = 0; i < cntSubscribers; i++)
+            {
+                Console.Write("Введіть ім'я абонента: ");
+                string sName = Console.ReadLine();
+                Console.Write("Введіть номер телефону: ");
+                string sPhoneNumber = Console.ReadLine();
+                Console.Write("Введіть адресу абонента: ");
+                string sAddress = Console.ReadLine();
+                Console.Write("Чи є у абонента Інтернет? (y-так, n-ні): ");
+                ConsoleKeyInfo keyHasInternet = Console.ReadKey();
+                Console.WriteLine();
+                Console.Write("Введіть кількість хвилин дзвінків на місяць: ");
+                string sCallMinutesPerMonth = Console.ReadLine();
+                Console.Write("Введіть кількість SMS на місяць: ");
+                string sSMSPerMonth = Console.ReadLine();
+                Console.Write("Введіть щомісячну плату: ");
+                string sMonthlyFee = Console.ReadLine();
+                Console.WriteLine();
+
+                Subscriber theSubscriber = new Subscriber();
+                theSubscriber.Name = sName;
+                theSubscriber.PhoneNumber = sPhoneNumber;
+                theSubscriber.Address = sAddress;
+                theSubscriber.HasInternet = keyHasInternet.Key == ConsoleKey.Y ? true : false;
+                theSubscriber.CallMinutesPerMonth = int.Parse(sCallMinutesPerMonth);
+                theSubscriber.SMSPerMonth = int.Parse(sSMSPerMonth);
+                theSubscriber.MonthlyFee = double.Parse(sMonthlyFee);
+
+                arrSubscribers[i] = theSubscriber;
+            }
+
+            foreach (Subscriber s in arrSubscribers)
+            {
+                Console.WriteLine();
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Дані про абонента: " + s.Name);
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Номер телефону: " + s.PhoneNumber);
+                Console.WriteLine("Адреса: " + s.Address);
+                Console.WriteLine("Інтернет: " + (s.HasInternet ? "Підключено" : "Немає"));
+                Console.WriteLine("Кількість хвилин дзвінків на місяць: " + s.CallMinutesPerMonth.ToString());
+                Console.WriteLine("Кількість SMS на місяць: " + s.SMSPerMonth.ToString());
+                Console.WriteLine("Щомісячна плата: " + s.MonthlyFee.ToString("0.00"));
+                Console.WriteLine("Вартість хвилини дзвінка: " + s.CostPerMinute.ToString("0.00"));
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Lab2Lib/Subscriber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Lib
+{
+    public class Subscriber
+    {
+ 
+            public string Name;            // Ім'я абонента
+            public string PhoneNumber;      // Номер телефону
+            public string Address;          // Адреса абонента
+            public bool HasInternet;        // Чи є у абонента підключення до Інтернету
+            public int CallMinutesPerMonth; // Кількість хвилин дзвінків на місяць
+            public int SMSPerMonth;         // Кількість SMS на місяць
+            public double MonthlyFee;       // Щомісячна плата за послуги
+            public double CostPerMinute
+            {
+                get
+                {
+                    return GetCostPerMinute();
+                }
+            }
+
+            public double GetCostPerMinute()
+            {
+                if (CallMinutesPerMonth == 0)
+                {
+                    return 0;
+                }
+                return MonthlyFee / CallMinutesPerMonth;
+            }
+        }
+    }
